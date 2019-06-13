632adc28eb521ef4c3e766698b911615ccdb1cb0
Tim <timstudt@gmail.com>
2019-06-13 10:47:43 +0200

    update travis with Danger

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -7,5 +7,3 @@ set -o pipefail
 
 xcodebuild -scheme MovieDBTests -workspace "$WORKSPACE" -destination "$PLATFORM" test | xcpretty -f `xcpretty-json-formatter`;
 xcodebuild -scheme MovieDBDataTests -workspace "$WORKSPACE" -destination "$PLATFORM" test | xcpretty -f `xcpretty-json-formatter`;
-
-bundle exec danger dry_run #TODO remove for CI
